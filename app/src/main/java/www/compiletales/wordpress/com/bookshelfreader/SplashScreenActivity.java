8af4e2cd8675263340f4770dc1b39bc0914f4151
LakshyaKhatri <lakshyak67@gmail.com>
2019-04-04 04:38:57 +0530

    Add two new activity and test data transfer

--- app/src/main/java/www/compiletales/wordpress/com/bookshelfreader/SplashScreenActivity.java
+++ app/src/main/java/www/compiletales/wordpress/com/bookshelfreader/SplashScreenActivity.java
@@ -3,11 +3,11 @@ package www.compiletales.wordpress.com.bookshelfreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
