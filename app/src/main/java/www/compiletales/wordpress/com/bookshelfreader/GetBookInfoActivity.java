3c102c173b38593f3bd385e7823099542228bd98
LakshyaKhatri <lakshyak67@gmail.com>
2019-04-06 02:56:12 +0530

    Add activity GetBookInfoActivity

---
+++ app/src/main/java/www/compiletales/wordpress/com/bookshelfreader/GetBookInfoActivity.java
@@ -0,0 +1,13 @@
+package www.compiletales.wordpress.com.bookshelfreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetBookInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_book_info);
+    }
+}
